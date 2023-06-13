53589ac5ef89acb30163a3f39085fd631400d96c
Mariane Candian <mariane.candian@gmail.com>
2023-06-13 17:14:12 -0300

    feat: criando função process do aquivo file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name = path_file.split("/")[-1]
+
+    if any(item["nome_do_arquvo"] == file_nema for item in instance._data):
+        return None
+
+    lines = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(dict)
+
+    print(dict)
 
 
 def remove(instance):
