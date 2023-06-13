50de927302f244c1895c4344e14b7810f150b7e4
Mariane Candian <mariane.candian@gmail.com>
2023-06-13 16:08:41 -0300

    feat: criando a função txt_importer no arquivo file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(file_path):
+    if not file_path.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    try:
+        with open(file_path, "r") as file:
+            lines = file.read().splitlines()
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} não encontrado", file=sys.stderr)
+        return []
