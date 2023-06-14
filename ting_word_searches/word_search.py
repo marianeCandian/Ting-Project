2b607472b49964f04c3043c1dde74b1f87c09825
Mariane Candian <mariane.candian@gmail.com>
2023-06-14 15:04:54 -0300

    feat: criando funções exists_word e search_by_word no arquivo word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,47 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+
+    for item in instance._data:
+        file_name = item["nome_do_arquivo"]
+        lines = item["linhas_do_arquivo"]
+        file_occurrences = []
+
+        for i, line in enumerate(lines):
+            if word.lower() in line.lower():
+                file_occurrences.append({"linha": i + 1})
+
+        if file_occurrences:
+            file_info = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": file_occurrences,
+            }
+            occurrences.append(file_info)
+
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+
+    for item in instance._data:
+        file_name = item["nome_do_arquivo"]
+        lines = item["linhas_do_arquivo"]
+        file_occurrences = []
+
+        for i, line in enumerate(lines):
+            if word.lower() in line.lower():
+                line_content = line.strip()
+                file_occurrences.append(
+                    {"linha": i + 1, "conteudo": line_content}
+                )
+
+        if file_occurrences:
+            file_info = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": file_occurrences,
+            }
+            occurrences.append(file_info)
+
+    return occurrences
